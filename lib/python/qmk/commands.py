69a28867663a85b84380b41d02ba521e2cd235c0
Nick Brassel <nick@tzarc.org>
2024-05-19 13:46:10 +1000

    Apply suggestions from code review
    Co-authored-by: Joel Challis <git@zvecr.com>

--- lib/python/qmk/commands.py
+++ lib/python/qmk/commands.py
@@ -54,9 +54,7 @@ def parse_configurator_json(configurator_file):
         cli.log.error(f'Invalid JSON keymap: {configurator_file} : {e.message}')
         exit(1)
 
-    keyboard = None
-    if 'keyboard' in user_keymap:
-        keyboard = user_keymap['keyboard']
+    keyboard = user_keymap.get('keyboard', None)
     aliases = keyboard_alias_definitions()
 
     while keyboard in aliases:
